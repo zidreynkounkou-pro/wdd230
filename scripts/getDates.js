fb1ae271bce524c126d7d625612b884ef69c04b8
zidreynkounkou-pro <relgazidreynkounkou@mail.com>
2024-01-20 20:33:52 +0100

    Finished with the dark mode.

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -19,5 +19,34 @@ const button = document.querySelector('#menu');
 button.addEventListener('click', () =>{
   nav.classList.toggle('show');
   button.classList.toggle('show');
-})
+});
+
+// Dark mode
+
+const dark = document.querySelector('#dark');
+const main = document.querySelector('main');
+const h2 = document.querySelector('h2');
+const h2Ex = document.querySelector('#h2Ex');
+const h1 = document.querySelector('h1');
+
+dark.addEventListener('click', () => {
+  if (dark.innerHTML.includes('🕶️')){
+    main.style.backgroundColor = 'black';
+    main.style.color = 'white';
+    h2.style.backgroundColor = '#031926';
+    h2Ex.style.backgroundColor = '#031926';
+    h1.style.color = 'white';
+    dark.innerHTML = '🔆';
+  }
+
+  else {
+    dark.innerHTML = '🕶️';
+    h1.style.color = '#33658a';
+    main.style.backgroundColor = 'white';
+    h2Ex.style.backgroundColor = '#468189';
+    h2.style.backgroundColor = '#468189';
+    main.style.color = 'black';
+    
+  }
+});
 
