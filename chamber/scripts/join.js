b43a8e649d9c65996c00272d0b11d3508f4ec4fa
zidreynkounkou-pro <relgazidreynkounkou@mail.com>
2024-02-08 20:35:19 +0100

    join.js added

---
+++ chamber/scripts/join.js
@@ -0,0 +1,62 @@
+const currentDate = new Date().getFullYear();
+document.querySelector('#date').innerHTML = currentDate;
+
+const lastModified = document.querySelector('#lastModified');
+lastModified.innerHTML = `Last modified: ${document.lastModified}`;
+
+
+// Highlight the current link
+const current = window.location.href;
+  const  show = document.querySelectorAll('nav a');
+ show.forEach(function(link){
+  if(link.href === current){
+    link.style.backgroundColor = "#032b43";
+    link.style.fontSize = "19px";
+  }});
+
+
+/*
+   For example, you might set focus when a button is clicked:
+  document.getElementById("myButton").addEventListener("click", function() {
+       myInput.focus();
+   }); */
+
+// Hamburger
+const nav = document.querySelector('nav');
+const button = document.querySelector('#hamburger');
+
+function toggleNav() {
+  nav.classList.toggle('show');
+  button.classList.toggle('show');
+  console.log('Button clicked');
+}
+
+button.addEventListener('click', toggleNav);
+
+
+function checkWindowWidth() {
+  if (window.innerWidth > 984) {
+    // If window width is greater than 984pxe, remove the 'show' class
+    nav.classList.remove('show');
+    button.classList.remove('show');
+    button.removeEventListener('click', toggleNav);
+  } else {
+    // If window width is less than 984px and the event listener is not added, add it
+    if (window.innerWidth < 984) {
+      button.addEventListener('click', toggleNav);
+    }
+  }
+}
+
+// Add an event listener to check the window width when the window is resized
+window.addEventListener('resize', checkWindowWidth);
+
+// Week 04
+const timestamp = document.querySelector('#timestamp');
+
+window.onload = function(){
+  const date = new Date();
+  const now = date.toLocaleString();
+  timestamp.value = now;
+  console.log(now)
+}
