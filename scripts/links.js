5fde9aec3f5ffb2f775af15deb90fd48ff33fd99
zidreynkounkou-pro <relgazidreynkounkou@mail.com>
2024-02-14 20:04:21 +0100

    Team activity finished

--- scripts/links.js
+++ scripts/links.js
@@ -2,12 +2,43 @@ const baseURL = "https://zidreynkounkou-pro.github.io/wdd230/";
 const linksURL = "https://zidreynkounkou-pro.github.io/wdd230/data/links.json";
 
 async function getLinks() {
-  const response = await fetch(linksURL);
-  const data = await response.json();
-  if(response.ok){
-    console.log(data);
+  try {
+    const response = await fetch(linksURL);
+    if (response.ok) {
+      const data = await response.json();
+      displayLinks(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
   }
-  
 }
 
 getLinks();
+
+
+
+function displayLinks(data) {
+  const ul = document.querySelector('ul');
+
+  data.weeks.forEach(week => {
+    const weekNumber = week.week;
+    const li = document.createElement('li');
+    const weekLinks = document.createElement('span');
+    week.links.forEach((link, index) => {
+      const url = link.url;
+      const title = link.title;
+      const a = document.createElement('a');
+      a.href = url;
+      a.textContent = title;
+      weekLinks.appendChild(a);
+      if (index !== week.links.length - 1) {
+        weekLinks.appendChild(document.createTextNode(' | '));
+      }
+    });
+    li.textContent = `${weekNumber}: `;
+    li.appendChild(weekLinks);
+    ul.appendChild(li);
+  });
+}
